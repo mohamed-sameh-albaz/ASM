35889ee813b90bf097060933470d037167487306
Uderscore <162705423+Uderscore@users.noreply.github.com>
2024-12-03 00:43:11 +0200

    Done  with boundarys

--- breaker.asm
+++ breaker.asm
@@ -5,6 +5,7 @@
       c       db 9
       lenght  dw 50
       Bidth   dw 10
+      yorigin dw 180
       x       dw 120
       y       dw 180
       breaker db 5
@@ -14,56 +15,77 @@
 
 MAIN PROC
       ; Initialize data segment
-               MOV AX, @DATA
-               MOV DS, AX
+                 MOV AX, @DATA
+                 MOV DS, AX
          
-               mov ch,50
-               mov cl,10
-      loopmain:
-               MOV BH,1
-               mov ah,0
-               mov al,13h
-               int 10h
+                 mov ch,50
+                 mov cl,10
+      loopmain:  
+                 MOV BH,1
+                 mov ah,0
+                 mov al,13h
+                 int 10h
           
        
-      loop1:   
+      loop1:     
          
-               mov cx,x           ;Column
-               mov dx,y           ;Row
-               mov al,5           ;Pixel color
-               mov ah,0ch         ;Draw Pixel Command
-      back:    int 10h
-               inc cx
+                 mov cx,x            ;Column
+                 mov dx,y            ;Row
+                 mov al,5            ;Pixel color
+                 mov ah,0ch          ;Draw Pixel Command
+      back:      int 10h
+                 inc cx
            
-               cmp cx,endx
-               jnz back
-               inc dx
-               mov y,dx
+                 cmp cx,endx
+                 jnz back
+                 inc dx
+                 mov y,dx
             
-               cmp dx,endy
-               jnz loop1
+                 cmp dx,endy
+                 jnz loop1
 
-               mov ax,y
-               mov bx ,Bidth
-               sub ax,bx
+                 mov ax,yorigin
+                 mov y,ax
             
-               mov y,ax
+               
 
 
-      loop2:   
-               mov ah,0
-               int 16h
-               cmp al,0
-               jnz loop2
-      movr:    
-               mov ax ,endx
-               add ax,20
-               mov endx,ax
-               mov ax,x
-               add ax,20
-               mov x,ax
-               jmp loopmain
-      cont:    
+      loop2:     
+                 mov dx,20
+                 mov ah,0
+                 int 16h
+                 cmp ah,4Dh
+                 jz  movr
+                 cmp ah,4Bh
+                 jnz loop2
+                 mov dx,-20
+      movr:      
+             
+           
+                 mov ax ,endx
+                 add ax,dx
+                 mov endx,ax
+                 cmp ax,319
+                 ja  rightbound
+                 cmp ax,lenght
+                 ja  inbound
+                 mov ax,lenght
+                 mov x,0
+                 mov endx,ax
+                 jmp cont
+      rightbound:
+                 mov ax,lenght
+                 mov bx,319
+                 sub bx,ax
+                 mov endx,319
+
+      inbound:   
+                 mov ax,endx
+                 mov bx,lenght
+                 sub ax,bx
+                 mov x,ax
+               
+      cont:      jmp loopmain
 
 
 MAIN ENDP
